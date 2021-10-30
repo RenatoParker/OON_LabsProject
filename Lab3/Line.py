d2ac64a42b666d1b2f034c24df442df9b720cb3e
Renato Ambrosone <s279186@studenti.polito.it>
2021-10-30 18:26:23 +0200

    draaw

--- Lab3/Line.py
+++ Lab3/Line.py
@@ -2,15 +2,38 @@ class Line:
     def __init__(self, label, length):
         self._label = label
         self._length = length
-        self._successive = ""
+        self._successive = dict()
         print("New line created:", "\t", "Label: ", self._label, "\t", "Length:", self._length)
 
+    @property
+    def label(self):
+        return self._label
 
-    def latency_generation(self): float(self._length/(3.33564e-9*(2/3)))
+    @label.setter
+    def label(self, label):
+        self._label = label
+
+    @property
+    def length(self):
+        return self._length
+
+    @length.setter
+    def length(self, length):
+        self._length = length
+
+    @property
+    def successive(self):
+        return self._successive
+
+    @successive.setter
+    def successive(self, successive):
+        self._successive = successive
+
+    def latency_generation(self): float(self._length / (3.33564e-9 * (2 / 3)))
 
     def noise_generation(self, signal_power): 1e-9 * signal_power * self._length
 
-# Define a propagate method that updates the signal information modifying its
+    # Define a propagate method that updates the signal information modifying its
     # noise power and its latency and call the successive element propagate method,
     # accordingly to the specified path.
     def propagate(self, signal_information):
--- Lab3/Network.py
+++ Lab3/Network.py
@@ -2,11 +2,13 @@ import json
 from Node import Node
 from Line import Line
 import math
+import matplotlib.pyplot as plt
+
 
 class Network:
     def __init__(self):
-        nodeList = list()
-        lineList = list()
+        self._nodeList = list()
+        self._lineList = list()
         with open("../Resource/nodes.json", "r") as read_file:
             self._nodes = json.load(read_file)
             # print(self._nodes)
@@ -14,20 +16,75 @@ class Network:
                 nodeData = nodeValue
                 nodeData["label"] = nodeKey
                 new_node = Node(nodeData)
-                nodeList.append(new_node)
+                self._nodeList.append(new_node)
 
-            for node in nodeList:
+            for node in self._nodeList:
                 for connectedNode in node.connected_node:
                     label = node.label + connectedNode
-                    nodeConnected = next((x for x in nodeList if x.label == connectedNode))
+                    nodeConnected = next((x for x in self._nodeList if x.label == connectedNode))
                     y1 = node.position[1]
                     y0 = nodeConnected.position[1]
                     x1 = node.position[0]
                     x0 = nodeConnected.position[0]
                     dist = math.hypot(y1 - y0, x1 - x0)
                     newLine = Line(label, dist)
-                    lineList.append(newLine)
+                    self._lineList.append(newLine)
+
+    # each node must have a dict of lines and each line must have a dictionary of a node
+    def connect(self):
+        for node in self._nodeList:
+           for line in self._lineList:
+               if line.label.startswith(node.label):
+                   node.successive[line.label]=line
+
+        for line in self._lineList:
+            for label in line.label:
+                node = next((x for x in self._nodeList if x.label == label))
+                line.successive[label] = node
 
 
+    def find_paths(self, labelA, labelB):
+        nodes = self._nodeList.copy()
+        paths = list()
+        while len(nodes) > 0:
+            path = []
+            crossed = list()
+            nodeA = next((x for x in self._nodeList if x.label == labelA))
+            nodes.remove(nodeA)
+            path.append(nodeA.label)
+            neighbors = nodeA.successive.keys()
+            for neighbor in neighbors:
+                self.find_paths(neighbor.label, labelB)
+
+    def recursive_find_paths(self , labelA, labelB, crossed):
+        return
+        # while len(nodes)> 1:
+
+
+    def propagate(self, signal_information):
+        return
+
+    def draw(self):
+        figure, axes = plt.subplots()
+        plt.xlim(-800000, 800000)
+        plt.ylim(-800000, 800000)
+        for node in self._nodeList:
+            print(node.position[0], node.position[1])
+            draw_circle = plt.Circle((node.position[0], node.position[1]), 30000, color='r')
+            axes.add_artist(draw_circle)
+
+        for line in self._lineList:
+            labelA = line.label[0]
+            labelB = line.label[1]
+            nodeA = next((x for x in self._nodeList if x.label == labelA))
+            nodeB = next((x for x in self._nodeList if x.label == labelB))
+            point1 = [nodeA.position[0], nodeA.position[1]]
+            point2 = [nodeB.position[0], nodeB.position[1]]
+            x_values = [point1[0], point2[0]]
+            y_values = [point1[1], point2[1]]
+            plt.plot(x_values, y_values, color="b")
+        plt.show()
 
 net = Network()
+net.connect()
+net.draw()
