26abc799ed4b4cce9baf801ef06fde408ba38a54
Renato Ambrosone <s279186@studenti.polito.it>
2022-03-03 10:47:33 +0100

    final commit

--- main.py
+++ main.py
@@ -57,186 +57,185 @@ if __name__ == '__main__':
     plt.savefig("./pic/SNROnLinks", dpi=200.0)
     plt.show()
 
-    # todo confronta in vari transceiver
-    # for fom in ["latency", "snr"]:
-    #     simulationResultsFull = []
-    #     simulationResultsNotFull = []
-    #     emme = range(1, 100)
-    #     for m in emme:
-    #         traffic_matrix = []
-    #         for indexRow, valueRow in enumerate(nodes_not_full):
-    #             row = []
-    #             for indexCol, valueCol in enumerate(nodes_not_full):
-    #                 if indexRow == indexCol:
-    #                     row.append(0)
-    #                 else:
-    #                     row.append(100 * 10)
-    #             traffic_matrix.append(row)
-    #         simulationResultsFull.append(net.createAndManageConnections(traffic_matrix, fom))
-    #         simulationResultsNotFull.append(net_not_full.createAndManageConnections(traffic_matrix, fom))
-    #
-    #     for index, simulationResults in enumerate([simulationResultsFull, simulationResultsNotFull]):
-    #         if index == 0:
-    #             netType = "Full network"
-    #         if index == 1:
-    #             netType = "Not Full network"
-    #         avgBitrateAllocated = 0
-    #         avgAllocatedConnections = 0
-    #         avgBlockingEvent = 0
-    #
-    #         GSNRavgs = []
-    #         bitrateAllocated = []
-    #         allocatedConnections = []
-    #         blockingEvent = []
-    #         netIsSaturated = []
-    #         perLinkBitRateAvg = []
-    #         perLinkBitRateMin = []
-    #         perLinkBitRateMax = []
-    #
-    #         perLinkGSNRAvg = []
-    #         perLinkGSNRMin = []
-    #         perLinkGSNRMax = []
-    #
-    #         perLinkLatencyAvg = []
-    #         perLinkLatencyMin = []
-    #         perLinkLatencyMax = []
-    #
-    #         for res in simulationResults:
-    #             print(res)
-    #             avgBitrateAllocated += res["bitrateAllocated"]
-    #             avgAllocatedConnections += res["allocatedConnections"]
-    #             avgBlockingEvent += res["blockingEvent"]
-    #
-    #             GSNRavgs.append(res["GSNRavg"])
-    #             bitrateAllocated.append(res["bitrateAllocated"])
-    #             allocatedConnections.append(res["allocatedConnections"])
-    #             blockingEvent.append(res["blockingEvent"])
-    #             netIsSaturated.append(res["netIsSaturated"])
-    #
-    #             perLinkBitRateAvg.append(res["perLinkBitRateAvg"])
-    #             perLinkBitRateMin.append(res["perLinkBitRateMin"])
-    #             perLinkBitRateMax.append(res["perLinkBitRateMax"])
-    #
-    #             perLinkGSNRAvg.append(res["perLinkGSNRAvg"])
-    #             perLinkGSNRMin.append(res["perLinkGSNRMin"])
-    #             perLinkGSNRMax.append(res["perLinkGSNRMax"])
-    #
-    #             perLinkLatencyAvg.append(res["perLinkLatencyAvg"])
-    #             perLinkLatencyMin.append(res["perLinkLatencyMin"])
-    #             perLinkLatencyMax.append(res["perLinkLatencyRMax"])
-    #
-    #         avgBitrateAllocated /= len(simulationResults)
-    #         avgAllocatedConnections /= len(simulationResults)
-    #         avgBlockingEvent /= len(simulationResults)
-    #
-    #         print(" ====== Bitrate Allocated ====== ")
-    #         print("Average: ", avgBitrateAllocated)
-    #         print("Min: ", min(bitrateAllocated))
-    #         print("Max: ", max(bitrateAllocated))
-    #
-    #         print(" ====== GSNR ======")
-    #         print("Average: ", sum(GSNRavgs)/len(GSNRavgs))
-    #         print("Min: ", min(GSNRavgs))
-    #         print("Max: ", max(GSNRavgs))
-    #
-    #         print(" ====== Connection Allocated ======")
-    #         print("Average: ", sum(allocatedConnections)/len(allocatedConnections))
-    #         print("Min: ", min(allocatedConnections))
-    #         print("Max: ", max(allocatedConnections))
-    #
-    #         print(" ====== Blocking Event ======")
-    #         print("Average: ", sum(blockingEvent)/len(blockingEvent))
-    #         print("Min: ", min(blockingEvent))
-    #         print("Max: ", max(blockingEvent))
-    #
-    #         print(" ====== Link Capacity ======")
-    #         print("Average: ", sum(perLinkBitRateAvg)/len(perLinkBitRateAvg))
-    #         print("Min: ", min(perLinkBitRateAvg))
-    #         print("Max: ", max(perLinkBitRateAvg))
-    #
-    #
-    #         print(" ====== Link GSNR ======")
-    #         print("Average: ", sum(perLinkGSNRAvg)/len(perLinkGSNRAvg))
-    #         print("Min: ", min(perLinkGSNRAvg))
-    #         print("Max: ", max(perLinkGSNRAvg))
-    #
-    #         print(" ====== Link Latency ======")
-    #         print("Average: ", sum(perLinkLatencyAvg)/len(perLinkLatencyAvg))
-    #         print("Min: ", min(perLinkLatencyAvg))
-    #         print("Max: ", max(perLinkLatencyAvg))
-    #
-    #         plt.hist(GSNRavgs, bins=50)
-    #         plt.gca().set(title='GSNR average Histogram ' + fom + ' ' + netType, ylabel='Frequency')
-    #         plt.savefig('./pic/GSNR average Histogram ' + fom + ' ' + netType, dpi=200.0)
-    #         plt.show()
-    #
-    #
-    #         print("bit rate")
-    #         print(bitrateAllocated)
-    #         plt.hist(bitrateAllocated, bins=50)
-    #         plt.gca().set(title='bitrate Allocated Histogram ' + fom + ' ' + netType, ylabel='Frequency')
-    #         plt.savefig('./pic/bitrate Allocated Histogram ' + fom + ' ' + netType, dpi=200.0)
-    #         plt.show()
-    #
-    #         plt.hist(allocatedConnections, bins=50)
-    #         plt.gca().set(title='allocated Connections  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
-    #         plt.savefig('./pic/allocated Connections  Histogram  ' + fom + ' ' + netType, dpi=200.0)
-    #         plt.show()
-    #
-    #         plt.hist(blockingEvent, bins=50)
-    #         plt.gca().set(title='blockingEvent average Histogram ' + fom + ' ' + netType, ylabel='Frequency')
-    #         plt.savefig('./pic/blockingEvent average Histogram ' + fom + ' ' + netType, dpi=200.0)
-    #         plt.show()
-    #
-    #         plt.hist(perLinkBitRateAvg, bins=20)
-    #         plt.gca().set(title='perLinkBitRate average  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
-    #         plt.savefig('./pic/perLinkBitRate average  Histogram   ' + fom + ' ' + netType, dpi=200.0)
-    #         plt.show()
-    #
-    #         plt.hist(perLinkBitRateMin, bins=20)
-    #         plt.gca().set(title='perLinkBitRate min  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
-    #         plt.savefig('./pic/perLinkBitRate min  Histogram  ' + fom + ' ' + netType, dpi=200.0)
-    #         plt.show()
-    #
-    #         plt.hist(perLinkBitRateMax, bins=20)
-    #         plt.gca().set(title='perLinkBitRate max  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
-    #         plt.savefig('./pic/perLinkBitRate max  Histogram   ' + fom + ' ' + netType, dpi=200.0)
-    #         plt.show()
-    #         print("as")
-    #
-    #         print(perLinkGSNRAvg)
-    #
-    #         plt.hist(perLinkGSNRAvg, bins=20)
-    #         plt.gca().set(title='perLinkGSNRAvg average  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
-    #         plt.savefig('./pic/perLinkGSNRAvg average  Histogram  ' + fom + ' ' + netType, dpi=200.0)
-    #         plt.show()
-    #
-    #         plt.hist(perLinkGSNRMin, bins=20)
-    #         plt.gca().set(title='perLinkGSNRMin min  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
-    #         plt.savefig('./pic/perLinkGSNRMin min  Histogram   ' + fom + ' ' + netType, dpi=200.0)
-    #         plt.show()
-    #
-    #         plt.hist(perLinkGSNRMax, bins=20)
-    #         plt.gca().set(title='perLinkGSNRMax max  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
-    #         plt.savefig('./pic/perLinkGSNRMax max  Histogram   ' + fom + ' ' + netType, dpi=200.0)
-    #         plt.show()
-    #
-    #
-    #         # plt.bar(emme, bitrateAllocated)
-    #         # plt.ylabel("Bitrate Allocated")
-    #         # plt.show()
-    #         # plt.bar(emme, allocatedConnections)
-    #         # plt.ylabel("Allocated Connections")
-    #         # plt.show()
-    #         # plt.bar(emme, blockingEvent)
-    #         # plt.ylabel("Blocking Events")
-    #         # plt.show()
-    #         # plt.bar(emme, GSNRavgs)
-    #         # plt.ylabel("GSNR average")
-    #         # plt.show()
-    #         # plt.bar(emme, netIsSaturated)
-    #         # plt.ylabel("netIsSaturated")
-    #         # plt.show()
+    for fom in ["latency", "snr"]:
+        simulationResultsFull = []
+        simulationResultsNotFull = []
+        emme = range(1, 100)
+        for m in emme:
+            traffic_matrix = []
+            for indexRow, valueRow in enumerate(nodes_not_full):
+                row = []
+                for indexCol, valueCol in enumerate(nodes_not_full):
+                    if indexRow == indexCol:
+                        row.append(0)
+                    else:
+                        row.append(100 * 10)
+                traffic_matrix.append(row)
+            simulationResultsFull.append(net.createAndManageConnections(traffic_matrix, fom))
+            simulationResultsNotFull.append(net_not_full.createAndManageConnections(traffic_matrix, fom))
+
+        for index, simulationResults in enumerate([simulationResultsFull, simulationResultsNotFull]):
+            if index == 0:
+                netType = "Full network"
+            if index == 1:
+                netType = "Not Full network"
+            avgBitrateAllocated = 0
+            avgAllocatedConnections = 0
+            avgBlockingEvent = 0
+
+            GSNRavgs = []
+            bitrateAllocated = []
+            allocatedConnections = []
+            blockingEvent = []
+            netIsSaturated = []
+            perLinkBitRateAvg = []
+            perLinkBitRateMin = []
+            perLinkBitRateMax = []
+
+            perLinkGSNRAvg = []
+            perLinkGSNRMin = []
+            perLinkGSNRMax = []
+
+            perLinkLatencyAvg = []
+            perLinkLatencyMin = []
+            perLinkLatencyMax = []
+
+            for res in simulationResults:
+                print(res)
+                avgBitrateAllocated += res["bitrateAllocated"]
+                avgAllocatedConnections += res["allocatedConnections"]
+                avgBlockingEvent += res["blockingEvent"]
+
+                GSNRavgs.append(res["GSNRavg"])
+                bitrateAllocated.append(res["bitrateAllocated"])
+                allocatedConnections.append(res["allocatedConnections"])
+                blockingEvent.append(res["blockingEvent"])
+                netIsSaturated.append(res["netIsSaturated"])
+
+                perLinkBitRateAvg.append(res["perLinkBitRateAvg"])
+                perLinkBitRateMin.append(res["perLinkBitRateMin"])
+                perLinkBitRateMax.append(res["perLinkBitRateMax"])
+
+                perLinkGSNRAvg.append(res["perLinkGSNRAvg"])
+                perLinkGSNRMin.append(res["perLinkGSNRMin"])
+                perLinkGSNRMax.append(res["perLinkGSNRMax"])
+
+                perLinkLatencyAvg.append(res["perLinkLatencyAvg"])
+                perLinkLatencyMin.append(res["perLinkLatencyMin"])
+                perLinkLatencyMax.append(res["perLinkLatencyRMax"])
+
+            avgBitrateAllocated /= len(simulationResults)
+            avgAllocatedConnections /= len(simulationResults)
+            avgBlockingEvent /= len(simulationResults)
+
+            print(" ====== Bitrate Allocated ====== ")
+            print("Average: ", avgBitrateAllocated)
+            print("Min: ", min(bitrateAllocated))
+            print("Max: ", max(bitrateAllocated))
+
+            print(" ====== GSNR ======")
+            print("Average: ", sum(GSNRavgs)/len(GSNRavgs))
+            print("Min: ", min(GSNRavgs))
+            print("Max: ", max(GSNRavgs))
+
+            print(" ====== Connection Allocated ======")
+            print("Average: ", sum(allocatedConnections)/len(allocatedConnections))
+            print("Min: ", min(allocatedConnections))
+            print("Max: ", max(allocatedConnections))
+
+            print(" ====== Blocking Event ======")
+            print("Average: ", sum(blockingEvent)/len(blockingEvent))
+            print("Min: ", min(blockingEvent))
+            print("Max: ", max(blockingEvent))
+
+            print(" ====== Link Capacity ======")
+            print("Average: ", sum(perLinkBitRateAvg)/len(perLinkBitRateAvg))
+            print("Min: ", min(perLinkBitRateAvg))
+            print("Max: ", max(perLinkBitRateAvg))
+
+
+            print(" ====== Link GSNR ======")
+            print("Average: ", sum(perLinkGSNRAvg)/len(perLinkGSNRAvg))
+            print("Min: ", min(perLinkGSNRAvg))
+            print("Max: ", max(perLinkGSNRAvg))
+
+            print(" ====== Link Latency ======")
+            print("Average: ", sum(perLinkLatencyAvg)/len(perLinkLatencyAvg))
+            print("Min: ", min(perLinkLatencyAvg))
+            print("Max: ", max(perLinkLatencyAvg))
+
+            plt.hist(GSNRavgs, bins=50)
+            plt.gca().set(title='GSNR average Histogram ' + fom + ' ' + netType, ylabel='Frequency')
+            plt.savefig('./pic/GSNR average Histogram ' + fom + ' ' + netType, dpi=200.0)
+            plt.show()
+
+
+            print("bit rate")
+            print(bitrateAllocated)
+            plt.hist(bitrateAllocated, bins=50)
+            plt.gca().set(title='bitrate Allocated Histogram ' + fom + ' ' + netType, ylabel='Frequency')
+            plt.savefig('./pic/bitrate Allocated Histogram ' + fom + ' ' + netType, dpi=200.0)
+            plt.show()
+
+            plt.hist(allocatedConnections, bins=50)
+            plt.gca().set(title='allocated Connections  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
+            plt.savefig('./pic/allocated Connections  Histogram  ' + fom + ' ' + netType, dpi=200.0)
+            plt.show()
+
+            plt.hist(blockingEvent, bins=50)
+            plt.gca().set(title='blockingEvent average Histogram ' + fom + ' ' + netType, ylabel='Frequency')
+            plt.savefig('./pic/blockingEvent average Histogram ' + fom + ' ' + netType, dpi=200.0)
+            plt.show()
+
+            plt.hist(perLinkBitRateAvg, bins=20)
+            plt.gca().set(title='perLinkBitRate average  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
+            plt.savefig('./pic/perLinkBitRate average  Histogram   ' + fom + ' ' + netType, dpi=200.0)
+            plt.show()
+
+            plt.hist(perLinkBitRateMin, bins=20)
+            plt.gca().set(title='perLinkBitRate min  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
+            plt.savefig('./pic/perLinkBitRate min  Histogram  ' + fom + ' ' + netType, dpi=200.0)
+            plt.show()
+
+            plt.hist(perLinkBitRateMax, bins=20)
+            plt.gca().set(title='perLinkBitRate max  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
+            plt.savefig('./pic/perLinkBitRate max  Histogram   ' + fom + ' ' + netType, dpi=200.0)
+            plt.show()
+            print("as")
+
+            print(perLinkGSNRAvg)
+
+            plt.hist(perLinkGSNRAvg, bins=20)
+            plt.gca().set(title='perLinkGSNRAvg average  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
+            plt.savefig('./pic/perLinkGSNRAvg average  Histogram  ' + fom + ' ' + netType, dpi=200.0)
+            plt.show()
+
+            plt.hist(perLinkGSNRMin, bins=20)
+            plt.gca().set(title='perLinkGSNRMin min  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
+            plt.savefig('./pic/perLinkGSNRMin min  Histogram   ' + fom + ' ' + netType, dpi=200.0)
+            plt.show()
+
+            plt.hist(perLinkGSNRMax, bins=20)
+            plt.gca().set(title='perLinkGSNRMax max  Histogram ' + fom + ' ' + netType, ylabel='Frequency')
+            plt.savefig('./pic/perLinkGSNRMax max  Histogram   ' + fom + ' ' + netType, dpi=200.0)
+            plt.show()
+
+
+            # plt.bar(emme, bitrateAllocated)
+            # plt.ylabel("Bitrate Allocated")
+            # plt.show()
+            # plt.bar(emme, allocatedConnections)
+            # plt.ylabel("Allocated Connections")
+            # plt.show()
+            # plt.bar(emme, blockingEvent)
+            # plt.ylabel("Blocking Events")
+            # plt.show()
+            # plt.bar(emme, GSNRavgs)
+            # plt.ylabel("GSNR average")
+            # plt.show()
+            # plt.bar(emme, netIsSaturated)
+            # plt.ylabel("netIsSaturated")
+            # plt.show()
 
     # todo confronta in vari transceiver
     for fom in ["latency", "snr"]:
